7dd2ad07ffa4689ccee8aa66311237bca060c658
Flan-B <s2291024@s.konan-u.ac.jp>
2025-04-30 05:02:02 +0900

    Update_2.1 index.py

--- lambda/index.py
+++ lambda/index.py
@@ -4,7 +4,15 @@ import os
 import boto3
 import re  # 正規表現モジュールをインポート
 from botocore.exceptions import ClientError
+from fastapi import FastAPI
+from pydantic import BaseModel
 
+app = FastAPI()
+class Message(BaseModel):
+    text: str
+@app.post("/predict")
+def predict(msg: Message):
+    return {"response": f"受け取ったもの: {msg.text}"}
 
 # Lambda コンテキストからリージョンを抽出する関数
 def extract_region_from_arn(arn):
